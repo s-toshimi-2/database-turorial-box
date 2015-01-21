2a59dc4ad9317c02faba58b58e0742e46ad65666
s-toshimi-2 <sionaion3@gmail.com>
2015-01-21 19:48:13 +0900

    refs #1 fixed postgresql initdb

--- modules/postgresql/manifests/config.pp
+++ modules/postgresql/manifests/config.pp
@@ -1,9 +1,16 @@
 class postgresql::config {
+    exec { 'init db':
+        user    => 'root',
+        command => '/sbin/service postgresql-9.3 initdb --encoding=UTF8 --no-locale',
+        unless  => '/usr/bin/test -f /var/lib/pgsql/9.3/data/pg_hba.conf'
+    }
+
     file { '/var/lib/pgsql/9.3/data/pg_hba.conf':
         ensure  => present,
         owner   => 'postgres',
         group   => 'postgres',
         mode    => '0600',
-        content => template('postgresql/pg_hba.conf')
+        content => template('postgresql/pg_hba.conf'),
+        require => Exec['init db']
     }
 }
